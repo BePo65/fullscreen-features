04ae1f469bf99a5f39f718acf89407a2ebe80868
Bernhard Pottler <bpottler@nexgo.de>
2024-02-11 15:01:11 +0100

    feat: first version

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # fullscreen-features
-Page to show available fullscreen api features for your browser
+
+Web page to show available fullscreen api features for your browser.
+
+
+![Version](https://img.shields.io/badge/version-1.0.0-blue.svg?cacheSeconds=2592000)
+[![License](https://img.shields.io/badge/License-MIT-yellow.svg)]
+
+## About The Project
+
+![Screenshot](assets/screenshot.jpg "Screenshot of the demo page")
+
+This project consists of a web page that shows the availability of the different elements of the web Fullscreen API.
+
+The page can also be used live.
+
+## License
+
+Distributed under the MIT License. See `LICENSE` for more information.
---
+++ src/scripts.js
@@ -0,0 +1,116 @@
+// Global variables
+const insertBeforeDiv = window.document.getElementById('fullscreen-features');
+const testDiv = window.document.createElement('div');
+
+// Add h2 tag for set of fullscreen api functions
+function addTitleTag(title) {
+  const newHeader = window.document.createElement('h2');
+  newHeader.classList.add('section-type');
+  const newContent = window.document.createTextNode(title ?? 'no Type');
+  newHeader.appendChild(newContent);
+  window.document.body.insertBefore(newHeader, insertBeforeDiv);
+}
+
+// Add container for group of features
+function addGroup(id) {
+  const newGroup = window.document.createElement('div');
+  newGroup.classList.add('feature-group');
+  newGroup.setAttribute('id', id);
+  window.document.body.insertBefore(newGroup, insertBeforeDiv);
+}
+
+// Add a row to the container with group features
+function addAvailabilityToGroup(groupId, functionName, isAvailable) {
+  const group = window.document.getElementById(groupId);
+
+  // Create column with name of the function
+  const newGroupEntryName = window.document.createElement('div');
+  const nameContent = window.document.createTextNode(functionName ?? 'no function name');
+  newGroupEntryName.appendChild(nameContent);
+  group.appendChild(newGroupEntryName);
+
+  // Create column with the availability of the function
+  const newGroupEntryState = window.document.createElement('div');
+  // Create element for availability image
+  const newGroupEntryStateImage = window.document.createElement('div');
+  if (isAvailable) {
+    newGroupEntryStateImage.classList.add('circle-green');
+  } else {
+    newGroupEntryStateImage.classList.add('circle-red');
+  }
+  newGroupEntryStateImage.innerHTML = '&nbsp;';
+  newGroupEntryState.appendChild(newGroupEntryStateImage);
+  // Create element for availability text
+  const newGroupEntryStateText = window.document.createElement('div');
+  newGroupEntryStateText.classList.add('feature-text');
+  const stateContentText = window.document.createTextNode(isAvailable ? 'available' : 'not available');
+  newGroupEntryStateText.appendChild(stateContentText);
+  newGroupEntryState.appendChild(newGroupEntryStateText);
+  // Add element with image and text to group
+  group.appendChild(newGroupEntryState);
+
+  // Insert row
+  window.document.body.insertBefore(group, insertBeforeDiv);
+}
+
+/**
+ * Main program
+ */
+const notAvailableString = 'not available';
+
+// browser detection
+window.document.getElementById('user-agent-header').innerHTML = navigator.userAgent;
+
+if (window.navigator.userAgentData) {
+  window.document.getElementById('browser-codename').innerHTML = window.navigator.userAgentData.appCodeName ?? notAvailableString;
+  window.document.getElementById('browser-name').innerHTML = window.navigator.userAgentData.appName ?? notAvailableString;
+  window.document.getElementById('browser-version').innerHTML = window.navigator.userAgentData.appVersion ?? notAvailableString;
+  window.document.getElementById('browser-platform').innerHTML = window.navigator.userAgentData.platform ?? notAvailableString;
+}
+
+// feature detection
+const fullscreenInterfaces = {
+  apiStandard: [
+    { functionName: 'requestFullscreen', baseElement: testDiv },
+    { functionName: 'exitFullscreen', baseElement: window.document },
+    { functionName: 'fullscreenElement', baseElement: window.document },
+    { functionName: 'fullscreenEnabled', baseElement: window.document }
+  ],
+  newWebkit: [
+		{ functionName: 'webkitRequestFullscreen', baseElement: testDiv },
+		{ functionName: 'webkitExitFullscreen', baseElement: window.document },
+		{ functionName: 'webkitFullscreenElement', baseElement: window.document },
+		{ functionName: 'webkitFullscreenEnabled', baseElement: window.document }
+  ],
+	oldWebKit: [
+		{ functionName: 'webkitRequestFullScreen', baseElement: testDiv },
+		{ functionName: 'webkitCancelFullScreen', baseElement: window.document },
+		{ functionName: 'webkitCurrentFullScreenElement', baseElement: window.document },
+		{ functionName: 'webkitCancelFullScreen', baseElement: window.document }
+  ],
+  oldMozilla: [
+		{ functionName: 'mozRequestFullScreen', baseElement: testDiv },
+		{ functionName: 'mozCancelFullScreen', baseElement: window.document },
+		{ functionName: 'mozFullScreenElement', baseElement: window.document },
+		{ functionName: 'mozFullScreenEnabled', baseElement: window.document }
+  ]
+}
+
+if (typeof window.document !== 'undefined') {
+  window.document.getElementById('error-message').classList.add('error-message-hide');
+
+  for (const key in fullscreenInterfaces) {
+    if (Object.hasOwnProperty.call(fullscreenInterfaces, key)) {
+      addTitleTag(key);
+      addGroup(key);
+      const element = fullscreenInterfaces[key];
+      element.forEach(featureEntry => {
+        const isAvailable = (featureEntry.functionName in featureEntry.baseElement);
+        addAvailabilityToGroup(key, featureEntry.functionName, isAvailable)
+      });
+    }
+  }
+} else {
+  window.document.getElementById('error-message').innerHTML = "Error: 'window.document' not available";
+  window.document.getElementById('error-message').classList.add('error-message-show');
+}
